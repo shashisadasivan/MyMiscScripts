158d46ca274074f3e6783b215384ba9ebd568ec0
shashisadasivan <shashi.sadasivan@gmail.com>
2019-06-11 15:54:47 +1200

    bulk rename files in folder

---
+++ Lightroom/BulkFileRename.ps1
@@ -0,0 +1,48 @@
+﻿Param(
+
+   [Parameter(Mandatory=$true)] #Full folder path where all the files exist
+   [string]$FolderPath,
+
+   [Parameter(Mandatory=$true)] #part of the file name that needs to be replaced
+   [string]$ReplaceFrom,
+
+   [Parameter(Mandatory=$true)] #replace ReplaceFrom to the value specified here
+   [string]$ReplaceTo,
+
+   [Parameter(Mandatory=$false)] #Should all the files be renumberd
+   [boolean]$RenumberAll = $false
+
+) #end param
+
+$files = Get-ChildItem -Path $FolderPath 
+
+foreach ($file in $files) {
+    $filename = $file.BaseName
+    if($filename -like ("*" + $ReplaceFrom + "*")) {
+        $newFileName = $filename -replace $ReplaceFrom, $ReplaceTo
+        $newFileName = $newFileName + $file.Extension
+    
+        "Renaming $filename to $newFilename"
+
+
+        Rename-Item -Path $file.PSPath -NewName $newFileName
+    }
+}
+
+if($RenumberAll = $true){
+    $totalFiles = $files.Count
+    "Renumbering all $totalFiles files"
+    
+    foreach ($file in $files) {
+        $filename = $file.BaseName
+        $newFilename = $filename
+        #TODO: add Numbering (remove existing if any)
+        $underscoreIndex = $newFilename.IndexOf("_")
+        $underscoreIndex
+        if($underscoreIndex > 0) {
+            $newFilename = $newFilename.Substring(0, $underscoreIndex + 1);
+        } 
+        #$newFilename
+    }
+    
+}
